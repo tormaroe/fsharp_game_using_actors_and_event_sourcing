29a05c3503924b7a45d2e74dc12b0c3a42da5928
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-12-20 15:43:09 +0100

    Refactored into ReportingAscii module

--- GameServer/Reporting.fs
+++ GameServer/Reporting.fs
@@ -1,49 +1,13 @@
 ﻿module Reporting
 
 open GameWorld
-open System.Text
-
-let getSym = function
-    | Points p -> p.ToString().[0]
-    | Transporter -> 'T'
-
-let getBoardArrays world =
-    let x, y = world.Size
-    let board = Array.init y (fun i -> Array.create x ' ')
-    let set (x,y) c = board.[y].[x] <- c
-    world.Players |> List.iter (fun p -> set p.Position 'P')
-    world.Items |> List.iter (fun (xy,it) -> getSym it |> set xy)
-    board
-
-let append (sb:StringBuilder) (s:string) = // usefull?
-    ignore <| sb.Append (s)
-
-let getAsciiBoard world =
-    let board = getBoardArrays world
-    let sb = StringBuilder ()
-    ignore <| sb.AppendFormat ("\nGAME: {0} VERSION: {1}\n", world.GameKey, world.Version)
-    ignore <| sb.Append("   +")
-    ignore <| sb.Append('-', board.[0].Length)
-    ignore <| sb.Append("+\n")
-    board |> Array.iteri 
-                (fun i line ->
-                    ignore <| sb.Append(i.ToString().PadLeft(3))
-                    ignore <| sb.Append ('|')
-                    ignore <| sb.Append (line)
-                    ignore <| sb.Append ("|\n"))
-    ignore <| sb.Append("   +")
-    ignore <| sb.Append('-', board.[0].Length)
-    ignore <| sb.Append("+\n")
-    world.Players 
-    |> Seq.sortBy (fun p -> p.Points)
-    |> Seq.iter (fun p -> 
-        ignore <| sb.AppendFormat 
-            ("PLAYER @{0}: {1}p - {2}\n", p.Position, p.Points, p.Id))
-    sb.ToString ()
+open ReportingAscii
 
 type Query =
     | UpdateState of GameWorld.World
+    | QueryWorld of AsyncReplyChannel<GameWorld.World>
     | QueryBoardASCII of AsyncReplyChannel<string>
+    | QueryPlayer of AsyncReplyChannel<GameWorld.Player>
 
 let private reportingActor = MailboxProcessor.Start(fun inbox ->
     let rec loop world =
@@ -61,10 +25,13 @@ let private reportingActor = MailboxProcessor.Start(fun inbox ->
 let post = reportingActor.Post
 let query = reportingActor.PostAndAsyncReply
 
+let private querySync qType =
+    query (fun chan -> qType (chan))
+    |> Async.RunSynchronously
 
 (* Query helper API *)
 
-let updateState world = UpdateState(world) |> reportingActor.Post
-
-let queryBoardASCII () = 
-    reportingActor.PostAndReply(fun chan -> QueryBoardASCII (chan))
+let updateState world = UpdateState(world) |> post
+let queryBoardASCII () = querySync QueryBoardASCII
+let queryPlayer () = querySync QueryPlayer
+let queryWorld () = querySync QueryWorld
---
+++ GameServer/ReportingAscii.fs
@@ -0,0 +1,42 @@
+﻿module ReportingAscii
+
+open GameWorld
+open System.Text
+
+let private getSym = function
+    | Points p -> p.ToString().[0]
+    | Transporter -> 'T'
+
+let private getBoardArrays world =
+    let x, y = world.Size
+    let board = Array.init y (fun i -> Array.create x ' ')
+    let set (x,y) c = board.[y].[x] <- c
+    world.Players |> List.iter (fun p -> set p.Position 'P')
+    world.Items |> List.iter (fun (xy,it) -> getSym it |> set xy)
+    board
+    
+let getAsciiBoard world =
+    let board = getBoardArrays world
+    let sb = StringBuilder ()
+    let appendString (s:string) = sb.Append (s) |> ignore
+    let appendCharAr (ca:char array) = sb.Append (ca) |> ignore
+    let appendRepeat (c:char) n = sb.Append (c, n) |> ignore
+    let appendFormat s (args:System.Object array) = sb.AppendFormat (s, args) |> ignore
+    appendFormat "\nGAME: {0} VERSION: {1}\n" [| world.GameKey ; world.Version |]
+    appendString "   +"
+    appendRepeat '-' board.[0].Length
+    appendString "+\n"
+    board |> Array.iteri 
+                (fun i line ->
+                    appendString <| i.ToString().PadLeft(3)
+                    appendString "|"
+                    appendCharAr line
+                    appendString "|\n")
+    appendString "   +"
+    appendRepeat '-' board.[0].Length
+    appendString "+\n"
+    world.Players 
+    |> Seq.sortBy (fun p -> p.Points)
+    |> Seq.iter (fun p -> 
+        appendFormat "PLAYER @{0}: {1}p - {2}\n" [| p.Position ; p.Points ; p.Id |])
+    sb.ToString ()
