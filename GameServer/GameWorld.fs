9b56daa64481dcbd63ef25006bc8a9fa1cfaff03
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-12-20 12:48:14 +0100

    Initialization / prompts / banner

--- GameServer/GameWorld.fs
+++ GameServer/GameWorld.fs
@@ -6,7 +6,7 @@ type Player = {
     Id : string
     Points : int
     Position : Coordinates
-}
+    }
 
 type ItemType =
     | Points of int
@@ -15,10 +15,18 @@ type ItemType =
 type Item = Coordinates * ItemType
 
 type World = {
+    GameKey: string
     Size: Coordinates
     Players : Player seq
     Items: Item seq 
-}
+    }
+
+let empty key size = {
+    GameKey = key
+    Size = size
+    Players = []
+    Items = [] 
+    }
 
 type Event =
     | PlayerSpawned of string * Coordinates
@@ -28,26 +36,28 @@ type SequencedEvent = int * Event // Usefull?
 
 let rnd = System.Random ()
 
+let rec nUnique n f acc =
+    let next = f ()
+    match n, Seq.tryFind ((=) next) acc with
+    | 0 , _      -> acc
+    | _ , None   -> nUnique (n - 1) f <| List.Cons (next, acc)
+    | _ , Some _ -> nUnique n f acc
+
+let uniqueRandomPos n (maxX, maxY) =
+    nUnique n (fun () -> rnd.Next (0, maxX), rnd.Next (0, maxY)) []
+
 let makeItem position =
     (match rnd.Next (0, 100) with
     | n when n < 70 -> rnd.Next (1, 10) |> Points
-    | _ -> Transporter), position
+    | _ -> Transporter)
+    , position
 
-let rec uniqueRandomPos n (maxX, maxY) acc =
-    let pos = rnd.Next (0, maxX), rnd.Next (0, maxY)
-    match n, Seq.tryFind ((=) pos) acc with
-    | 0 , _ -> acc
-    | _ , None -> List.Cons (pos, acc) 
-                  |> uniqueRandomPos (n - 1) (maxX, maxY)
-    | _ , Some _ -> uniqueRandomPos n (maxX, maxY) acc
-
-let split x s =
-    (Seq.take x s), (Seq.skip x s)
+let (-><-) s x = (Seq.take x s), (Seq.skip x s)
 
 let makeGameWorld names size itemCount =
     let playerCount = Seq.length names
-    let positions = uniqueRandomPos (itemCount + playerCount) size []
-    let playerPos, itemPos = positions |> split playerCount
+    let positions = uniqueRandomPos (itemCount + playerCount) size
+    let playerPos, itemPos = positions -><- playerCount
     Seq.append
         (Seq.map2 (fun n p -> PlayerSpawned(n, p)) names playerPos)
         (itemPos |> Seq.map makeItem |> Seq.map ItemSpawned)
--- GameServer/Program.fs
+++ GameServer/Program.fs
@@ -1,10 +1,91 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+open System
 open Http
+
+let hr () = 
+    printfn "------------------------------------------------------------------"
+
+let prompt (question : string) =
+    Console.Write question
+    Console.ReadLine().Trim()
+
+let promptInt = prompt >> Int32.Parse
+
+let promptGameKey () =
+    prompt "Enter a unique game identifier: "
+
+let promptPlayerNames () =
+    let rec loop i acc =
+        match prompt <| 
+              sprintf "Enter player name #%d (ENTER when done): " i 
+              with
+        | "" -> acc
+        | name -> name::acc |> loop (i + 1)
+    loop 1 []
+
+let promptBoardDimentions () =
+    let width = promptInt "Enter playing board width: "
+    let height = promptInt "Enter playing board height: "
+    width, height
+    
+let promptItemCount () =
+    promptInt "How many items do you want on the board? "
+
+let startupNew () =
+    hr ()
+    printfn "SETTING UP A NEW GAME..\n"
+    let gameKey = promptGameKey ()
+    let playerNames = promptPlayerNames ()
+    let size = promptBoardDimentions ()
+    let itemCount = promptItemCount ()
+    hr ()
+    let world = GameWorld.empty gameKey size
+    let initEvents = GameWorld.makeGameWorld 
+                        playerNames 
+                        size 
+                        itemCount
+    ()
+
+let startupReplay () =
+    Log.dbg "REPLAY not implemented!"
 
 [<EntryPoint>]
 let main argv = 
-    Log.dbg <| sprintf "%A" argv
+    if argv.Length <> 3
+    then
+        printfn "Usage: gameserver IPaddress port initcommand [gamekey]"
+        printfn ""
+        printfn "Examples: gameserver localhost 3000 new"
+        printfn "Examples: gameserver localhost 3000 replay foobar"
+        Environment.Exit 1
+
+    @"
+               \.   \.      __,-""-.__      ./   ./
+           \.   \`.  \`.-'"""" _,=""=._ """"`-.'/  .'/   ./
+            \`.  \_`-''      _,=""=._      ``-'_/  .'/
+             \ `-',-._   _.  _,=""=._  ,_   _.-,`-' /
+          \. /`,-',-._""""""  \ _,=""=._ /  """"""_.-,`-,'\ ./
+           \`-'  /    `-._  ""       ""  _.-'    \  `-'/
+           /)   (         \    ,-.    /         )   (\
+        ,-'""     `-.       \  /   \  /       .-'     ""`-,
+      ,'_._         `-.____/ /  _  \ \____.-'         _._`,
+     /,'   `.                \_/ \_/                .'   `,\
+    /'       )                  _                  (       `\
+            /   _,-'""`-.  ,++|T|||T|++.  .-'""`-,_   \
+           / ,-'        \/|`|`|`|'|'|'|\/        `-, \
+          /,'             | | | | | | |             `,\
+         /'               ` | | | | | '               `\
+                            ` | | | '
+                              ` | '
+       ***  R O B O  B O A R D     G A M E  S E R V E R  ***"
+    |> Console.WriteLine
+
+    match argv.[2] with
+    | "new" -> startupNew ()
+    | "replay" | "load" | "reload" -> startupReplay ()
+    | other -> failwith "Unknown initialization argument"
+
     listenForRequests argv.[0] argv.[1]
     0 // return an integer exit code
