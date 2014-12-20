fedcd94c9d045e3d00fa06e4709a04ade8880220
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-12-20 07:01:58 +0100

    Working HTTP server

---
+++ GameServer/Http.fs
@@ -0,0 +1,39 @@
+﻿module Http
+
+open System.Net
+open System.Web
+
+let respond (ctx:HttpListenerContext) (code, (result:string)) =
+    use res = ctx.Response
+    res.StatusCode <- code
+    let bytes = ctx.Request.ContentEncoding.GetBytes result
+    res.ContentLength64 <- bytes.LongLength
+    res.OutputStream.Write(bytes, 0, bytes.Length)
+
+let resourcePath (ctx:HttpListenerContext) =
+    HttpUtility.UrlDecode 
+        (ctx.Request.Url.AbsolutePath, ctx.Request.ContentEncoding)
+
+let dispatch (ctx:HttpListenerContext) =
+    match ctx |> resourcePath with
+    | "/"   -> 200, "HOME"
+    | _     -> 404, "UNKNOWN RESOURCE"
+    |> respond ctx 
+
+let handleRequest (ctx:HttpListenerContext) = 
+    async {
+        let req = ctx.Request
+        sprintf "[%s] %A" req.HttpMethod req.Url
+        |> Log.dbg
+        dispatch ctx
+    }
+
+let listenForRequests address port =
+    let listener = new HttpListener()
+    sprintf "http://%s:%s/" address port
+    |> listener.Prefixes.Add 
+    listener.Start()
+    while true do
+        listener.GetContext()
+        |> handleRequest 
+        |> Async.Start
---
+++ GameServer/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open Http
+
+[<EntryPoint>]
+let main argv = 
+    Log.dbg <| sprintf "%A" argv
+    listenForRequests argv.[0] argv.[1]
+    0 // return an integer exit code
